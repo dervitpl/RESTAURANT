04029f0f1edd3c4acaadd153f11c6a937e167e48
kacper mucha <kacpermucha420@gmail.com>
2022-09-13 20:37:59 +0200

    polaczenie z baza

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,29 @@ import express from 'express';
 import http from 'http';
 import mongoose from 'mongoose';
 import { config } from './config/config';
+import Logging from './library/Logging';
 
 const router = express();
 
 mongoose
-    .connect(config.mongo.url, { retryWrites: true, w: 'majority'})
+    .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
     .then(() => {
-        console.log('connected');
+        Logging.info('Mongo connected successfully.');
+        StartServer();
     })
-    .catch((error) => {
-        console.log(error);
-    });
+    .catch((error) => Logging.error(error));
+
+/** Only Start Server if Mongoose Connects */
+const StartServer = () => {
+    /** Log the request */
+    router.use((req, res, next) => {
+        /** Log the req */
+        Logging.info(`Incomming - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+
+        res.on('finish', () => {
+            /** Log the res */
+            Logging.info(`Result - METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}] - STATUS: [${res.statusCode}]`);
+        });
+
+        next();
+    })};
